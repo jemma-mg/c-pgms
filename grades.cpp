02defa22d5556e415d03ce33b119221c830e3fd2
jemma-mg <jemmamariyageorge@gmail.com>
2022-12-25 15:50:04 +0530

    grade display

--- grades.cpp
+++ grades.cpp
@@ -4,6 +4,34 @@
 using namespace std;
 int main()
 {
-    cout << "Hello World";
+    int mark = 50;
+    if (mark >= 90)
+    {
+        printf("A Grade");
+    }
+    else if (mark >= 80 && mark < 90)
+    {
+        printf("B Grade");
+    }
+    else if (mark >= 70 && mark < 80)
+    {
+        printf("C Grade");
+    }
+    else if (mark >= 60 && mark < 70)
+    {
+        printf("D Grade");
+    }
+    else if (mark >= 50 && mark < 60)
+    {
+        printf("E Grade");
+    }
+    else if (mark >= 40 && mark < 50)
+    {
+        printf("P Grade");
+    }
+    else
+    {
+        printf("F Grade");
+    }
     return 0;
 }
